8ee6d473955516bc94699b1db4b2b1e40f3ff5bb
PabloLnz <lorenzogonzalezpablo4@gmail.com>
2024-10-25 11:32:12 +0200

    Modificacion de vista

--- views/calculoNotas.view.php
+++ views/calculoNotas.view.php
@@ -24,3 +24,40 @@
             </div>
         </div>
     </div>
+    <!-- Si hay resultados, muestra las tablas y listados -->
+    <?php if (isset($data['resultado'])) { ?>
+    <div class="col-12">
+        <!-- Resultados por asignatura -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary">Resultados por asignatura</h6>
+            </div>
+            <div class="card-body">
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>Asignatura</th>
+                        <th>Media</th>
+                        <th>Suspensos</th>
+                        <th>Aprobados</th>
+                        <th>Nota más alta</th>
+                        <th>Nota mínima</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach ($data['resultado'] as $asignatura => $datos) { ?>
+                        <tr>
+                            <td><?php echo $asignatura; ?></td>
+                            <td><?php echo is_numeric($datos['media']) ? number_format($datos['media'], 2, ',') : $datos['media']; ?></td>
+                            <td><?php echo $datos['suspensos']; ?></td>
+                            <td><?php echo $datos['aprobados']; ?></td>
+                            <td><?php echo $datos['max']['alumno']; ?>: <?php echo round($datos['max']['nota']); ?></td>
+                            <td><?php echo $datos['min']['alumno']; ?>: <?php echo round($datos['min']['nota']); ?></td>
+                        </tr>
+                    <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+<?php } ?>
