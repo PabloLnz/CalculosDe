a4a255bc26812c6362cd3a4f22fd816fcfe9f075
PabloLnz <lorenzogonzalezpablo4@gmail.com>
2024-10-25 11:02:06 +0200

    Funcion checkError y la vista.

--- controllers/calculoNotas.controller.php
+++ controllers/calculoNotas.controller.php
@@ -8,6 +8,39 @@ if (!empty($_POST)) {
     $data['input']['texto'] = filter_var($_POST['texto'], FILTER_SANITIZE_SPECIAL_CHARS);
 }
 
+//Funcion para almacenar a los alumnos segun sus aprobados y suspensos
+function calcularListados(array $datos): array
+{
+    $aprobadoTodo = [];
+    $suspendidoAlMenosUna = [];
+    $promocionan = [];
+    $noPromocionan = [];
+
+    foreach ($datos as $asignaturas) {
+        foreach ($asignaturas as $alumno => $notas) {
+            $suspensos = 0;
+            foreach ($notas as $nota) {
+                if ($nota < 5) {
+                    $suspensos++;
+                }
+            }
+            if ($suspensos == 0) {
+                $aprobadoTodo[$alumno]=$alumno ;
+            } elseif ($suspensos == 1) {
+                $promocionan[$alumno]=$alumno;
+            } else {
+                $noPromocionan[$alumno]=$alumno;
+            }
+            if ($suspensos > 0) {
+                $suspendidoAlMenosUna[$alumno] = $alumno;
+            }
+        }
+    }
+    return ['aprobadoTodo' => $aprobadoTodo, 'promocionan' => $promocionan, 'noPromocionan' => $noPromocionan,'suspendidoAlMenosUna' => $suspendidoAlMenosUna];
+
+}
+
+
 //Funcion para comprobar que el JSON este bien formado
 function checkErrors(string $texto): array
 {
